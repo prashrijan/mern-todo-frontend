e62e5e4defa121fc6a3dc9eb5289fbd749b7cf43
Prashrijan <shresthaprashrijan@gmail.com>
2025-01-25 23:40:24 +1100

    add responsiveness

--- src/Components/TasksBreakdown.jsx
+++ src/Components/TasksBreakdown.jsx
@@ -4,7 +4,7 @@ import TaskItem from "./TaskItem";
 
 const TasksBreakdown = ({ tasks, changeStatus, handleDelete }) => {
   return (
-    <div className="w-full md:w-full bg-white p-12 overflow-y-auto">
+    <div className="w-full md:w-full p-12 overflow-y-auto">
       <h2 className="text-2xl font-semibold mb-6">Your Tasks</h2>
       {tasks.length > 0 ? (
         tasks.map((task) => (
